370142ee8937feca08ed1348a57b2993748ade4f
UladzimirIvanov <ivanouum@gmail.com>
2024-04-25 13:15:11 +0300

    Task1 result

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,20 @@
-package Task1;public class Main {
+package Task1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String userInput = scanner.nextLine();
+
+        Pattern abbr = Pattern.compile("[A-Z]{2,6}");
+        Matcher m = abbr.matcher(userInput);
+        System.out.println("Были найдены аббревиатуры: ");
+        while (m.find()){
+            System.out.println(m.group());
+        }
+    }
 }
